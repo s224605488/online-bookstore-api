019979549d05312315d90e5f3eeec3be0b73e0e5
s224605488 <s224605488@deakin.edu.au>
2025-05-23 13:06:34 +1000

    Initial commit of Online Bookstore API

---
+++ src/main/java/com/onlinebookstore/bookstore/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.onlinebookstore.bookstore.controller;
+
+import com.onlinebookstore.bookstore.model.Book;
+import com.onlinebookstore.bookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+}
---
+++ src/main/java/com/onlinebookstore/bookstore/model/Book.java
@@ -0,0 +1,17 @@
+package com.onlinebookstore.bookstore.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private double price;
+    private int stock;
+}
